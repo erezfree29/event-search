34ff901989313e04a90f6c7b34194a02b751a27a
Erez Friemagor <erezfree29@gmail.com>
2021-09-20 11:47:40 +0100

    linters

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,13 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+/* eslint-disable import/prefer-default-export */
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+import logger from 'redux-logger';
+
+const reducer = combineReducers({
 });
+
+export const store = createStore(
+  reducer,
+  applyMiddleware(thunk, logger),
+);
