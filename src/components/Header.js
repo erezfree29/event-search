237925dd8be959b4ebf4f222dacf8935cf9a573c
Erez Friemagor <erezfree29@gmail.com>
2021-09-20 20:30:43 +0100

    style images in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,14 @@
+/* eslint-disable no-unused-vars */
 import { NavLink } from 'react-router-dom';
 
-const Header = () => (
-  <>
+const baseURL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/7QlpK6CmgdU0hZt33zXJ/books';
+
+const Header = () => {
+  const hoover = () => {
+    alert(1);
+  };
+
+  return (
     <nav className="navbar">
       <div className="links">
         <div className="link">
@@ -27,16 +34,20 @@ const Header = () => (
         </div>
       </div>
       <div className="images">
-        <div className="image">
+        <div className="image gr">
           <img src="https://a5.behance.net/11aa46df22ab4e63bdd2e24a88f638e6c74a8f7d/img/galleries/icons/ribbons/1x/graphic-design.png?cb=264615658" alt="gr" />
         </div>
-        <div className="image">
+        <div>
           <img className="image" src="https://a5.behance.net/11aa46df22ab4e63bdd2e24a88f638e6c74a8f7d/img/galleries/icons/ribbons/1x/ui-ux.png?cb=264615658" alt="ui" />
-          <div className="two">2</div>
+          <div>2</div>
         </div>
       </div>
+      <div className="hide">
+        <div className="featured">FEATURED IN</div>
+        <div className="branding"><a href="https://www.behance.net/galleries/graphic-design/branding" style={{ color: 'blue' }}>Branding</a></div>
+      </div>
     </nav>
-  </>
-);
+  );
+};
 
 export default Header;
