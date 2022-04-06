525429471ac9349ee0e3898442dd1993a4b11cd3
Erez Friemagor <erezfree29@gmail.com>
2022-04-06 12:40:55 +0100

    linters

--- src/components/events.js
+++ src/components/events.js
@@ -1,20 +1,7 @@
 /* eslint-disable no-underscore-dangle */
-/* eslint-disable react/jsx-no-comment-textnodes */
-/* eslint-disable no-useless-concat */
-/* eslint-disable no-template-curly-in-string */
-/* eslint-disable max-len */
-/* eslint-disable no-empty */
-/* eslint-disable jsx-a11y/control-has-associated-label */
-/* eslint-disable react/jsx-indent */
-/* eslint-disable no-alert */
-/* eslint-disable jsx-a11y/alt-text */
-/* eslint-disable react/jsx-key */
-/* eslint-disable quotes */
-/* eslint-disable no-unused-vars */
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import { useSelector, useDispatch } from 'react-redux';
 import React, { useEffect, useState } from 'react';
-import Carousel from "react-elastic-carousel";
+import Carousel from 'react-elastic-carousel';
 import './events.css';
 
 function Events() {
@@ -22,9 +9,9 @@ function Events() {
 
   const fetchItems = async () => {
     const query = document.getElementById('query').value;
-    const beginUrl = "https://app.ticketmaster.com/discovery/v2/events.json?";
+    const beginUrl = 'https://app.ticketmaster.com/discovery/v2/events.json?';
     const keyWord = `keyword=${query}`;
-    let endUrl = "&dmaId=602&apikey=MOnDwV7JkArTT9CYE0NA50dUQeM1Os1i";
+    let endUrl = '&dmaId=602&apikey=MOnDwV7JkArTT9CYE0NA50dUQeM1Os1i';
     let url = beginUrl + keyWord + endUrl;
     if (document.getElementById('music').checked) {
       endUrl = `&classificationName=music&${endUrl}`;
@@ -89,16 +76,16 @@ function Events() {
 
   const onSubmit = (e) => {
     fetchItems();
-    document.querySelector('.carousel').style.setProperty("display", "block", "important");
+    document.querySelector('.carousel').style.setProperty('display', 'block', 'important');
     e.preventDefault();
   };
 
   useEffect(() => {
     if (localStorage.getItem('events') !== null) {
-      const storedNames = JSON.parse(localStorage.getItem("events"));
+      const storedNames = JSON.parse(localStorage.getItem('events'));
       setEvents(storedNames);
     } else {
-      document.querySelector('.carousel').style.setProperty("display", "none", "important");
+      document.querySelector('.carousel').style.setProperty('display', 'none', 'important');
     }
   }, []);
 
@@ -106,129 +93,132 @@ function Events() {
     <div>
       <div className="d-flex justify-content-center"><h1>Events matching &quot;London&quot;</h1></div>
       <div className="recepies">
-      <section className=" d-flex justify-content-center">
-     <div className="filter d-flex justify-content-center">
-    <fieldset>
-        <div className="type d-flex justify-content-center">
-            <form onSubmit={onSubmit} className="mt-2 p-3">
-          <div className="d-flex justify-content-center">
-            <div className="form-group w-50 fg">
-              <div className="input_button">
-              <div className="d-flex justify-content-center">
-              <input
-                className="mb-3"
-                id="query"
-                name="q"
-                type="search"
-                autoComplete="off"
-                maxLength="50"
-                placeholder="Search artist, event, venue, location..."
-              />
-              </div>
-                  <div className="d-flex justify-content-center"><button type="submit" className="btn btn-primary">Find</button></div>
-              </div>
-            </div>
-          </div>
-<div className="choice d-flex justify-content-center">
+        <section className=" d-flex justify-content-center">
+          <div className="filter d-flex justify-content-center">
+            <fieldset>
+              <div className="type d-flex justify-content-center">
+                <form onSubmit={onSubmit} className="mt-2 p-3">
+                  <div className="d-flex justify-content-center">
+                    <div className="form-group w-50 fg">
+                      <div className="input_button">
+                        <div className="d-flex justify-content-center">
+                          <input
+                            className="mb-3"
+                            id="query"
+                            name="q"
+                            type="search"
+                            autoComplete="off"
+                            maxLength="50"
+                            placeholder="Search artist, event, venue, location..."
+                          />
+                        </div>
+                        <div className="d-flex justify-content-center"><button type="submit" className="btn btn-primary">Find</button></div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="choice d-flex justify-content-center">
                     <div className="browse d-flex justify-content-between">
 
-                        <div className="mr-5">
-                            <div>
+                      <div className="mr-5">
+                        <div>
 
-                                <input className="mr-1" type="checkbox" value="2" name="c" id="dance" />
-                                <label htmlFor="category-2">Dance And Clubs</label>
+                          <input className="mr-1" type="checkbox" value="2" name="c" id="dance" />
+                          <label htmlFor="category-2">Dance And Clubs</label>
 
-                            </div>
-                            <div>
-
-                                <input className="mr-1" type="checkbox" value="3" name="c" id="music" />
-                                <label htmlFor="category-3">Music</label>
-
-                            </div>
-                            <div>
-
-                                <input className="mr-1" type="checkbox" value="5" name="c" id="tribute" />
-                                <label htmlFor="category-5">Tribute</label>
-
-                            </div>
-                            <div>
-
-                                <input className="mr-1" type="checkbox" value="92" name="c" id="film" />
-                                <label htmlFor="category-92">Film</label>
-
-                            </div>
-                            <div>
-
-                                <input className="mr-1" type="checkbox" value="40" name="c" id="art" />
-                                <label htmlFor="category-40">Art</label>
-
-                            </div>
                         </div>
                         <div>
-                            <div>
 
-                                <input className="mr-1" type="checkbox" value="14" name="c" id="festival" />
-                                <label htmlFor="category-14">Festival</label>
+                          <input className="mr-1" type="checkbox" value="3" name="c" id="music" />
+                          <label htmlFor="category-3">Music</label>
 
-                            </div>
-                            <div>
-
-                                <input className="mr-1" type="checkbox" value="27" name="c" id="special" />
-                                <label htmlFor="category-27">Special Events</label>
-
-                            </div>
-                            <div>
-
-                                <input className="mr-1" type="checkbox" value="76" name="c" id="comedy" />
-                                <label htmlFor="category-76">Comedy</label>
-
-                            </div>
-                            <div>
-
-                                <input className="mr-1" type="checkbox" value="18" name="c" id="talk" />
-                                <label htmlFor="category-18">Talk</label>
-
-                            </div>
-                            <div>
-
-                                <input className="mr-1" type="checkbox" value="246" name="c" id="sports" />
-                                <label htmlFor="category-246">Sport</label>
-
-                            </div>
                         </div>
+                        <div>
+
+                          <input className="mr-1" type="checkbox" value="5" name="c" id="tribute" />
+                          <label htmlFor="category-5">Tribute</label>
+
+                        </div>
+                        <div>
+
+                          <input className="mr-1" type="checkbox" value="92" name="c" id="film" />
+                          <label htmlFor="category-92">Film</label>
+
+                        </div>
+                        <div>
+
+                          <input className="mr-1" type="checkbox" value="40" name="c" id="art" />
+                          <label htmlFor="category-40">Art</label>
+
+                        </div>
+                      </div>
+                      <div>
+                        <div>
+
+                          <input className="mr-1" type="checkbox" value="14" name="c" id="festival" />
+                          <label htmlFor="category-14">Festival</label>
+
+                        </div>
+                        <div>
+
+                          <input className="mr-1" type="checkbox" value="27" name="c" id="special" />
+                          <label htmlFor="category-27">Special Events</label>
+
+                        </div>
+                        <div>
+
+                          <input className="mr-1" type="checkbox" value="76" name="c" id="comedy" />
+                          <label htmlFor="category-76">Comedy</label>
+
+                        </div>
+                        <div>
+
+                          <input className="mr-1" type="checkbox" value="18" name="c" id="talk" />
+                          <label htmlFor="category-18">Talk</label>
+
+                        </div>
+                        <div>
+
+                          <input className="mr-1" type="checkbox" value="246" name="c" id="sports" />
+                          <label htmlFor="category-246">Sport</label>
+
+                        </div>
+                      </div>
                     </div>
-</div>
+                  </div>
 
-            </form>
-        </div>
-        <div className="choice d-flex justify-content-center" />
-
-    </fieldset>
-     </div>
-      </section>
-        <div className="carousel">
-        <Carousel>
-          {events.map((event) => (
-            <>
-              <div className="event">
-                <div>click to see the event</div>
-                <a href={event.url}>
-                <img src={event.images[0].url} />
-                </a>
-                <div>
-{event._embedded.venues[0].name}
-{' '}
-                </div>
-<div>{event.dates.start.localDate}</div>
-<div>{event.dates.start.localTime.substring(0, event.dates.start.localTime.length - 3)}</div>
-
-                <div className="label_name">{event.name}</div>
+                </form>
               </div>
-              {}
+              <div className="choice d-flex justify-content-center" />
 
-            </>
-          ))}
-        </Carousel>
+            </fieldset>
+          </div>
+        </section>
+        <div className="carousel">
+          <Carousel>
+            {events.map((event) => (
+              <>
+                <div className="event">
+                  <div>click to see the event</div>
+                  <a href={event.url}>
+                    <img src={event.images[0].url} alt="event" />
+                  </a>
+                  <div>
+                    {event._embedded.venues[0].name}
+                    {' '}
+                  </div>
+                  <div>{event.dates.start.localDate}</div>
+                  <div>
+                    {event.dates.start.localTime.substring(0, event.dates.start
+                      .localTime.length - 3)}
+                  </div>
+
+                  <div className="label_name">{event.name}</div>
+                </div>
+                {}
+
+              </>
+            ))}
+          </Carousel>
         </div>
 
       </div>
